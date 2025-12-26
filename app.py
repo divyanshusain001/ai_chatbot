46eeb0d5ffbcce3ed4464b70a7d77625e4936c3a
divyanshusain001 <165560980+divyanshusain001@users.noreply.github.com>
2025-12-26 12:00:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
-import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
 
 # Load environment variables
 load_dotenv()
